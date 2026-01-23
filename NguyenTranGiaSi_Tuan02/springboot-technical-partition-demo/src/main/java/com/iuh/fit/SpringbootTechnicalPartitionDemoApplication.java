8e7569ebd5d3a1c5aeddb14d136aae9a0918f4f6
giasinguyen <giasinguyentran@gmail.com>
2026-01-23 20:14:10 +0700

    ADD: Initialize Spring Boot project with Maven wrapper, configuration files, and basic application structure

---
+++ NguyenTranGiaSi_Tuan02/springboot-technical-partition-demo/src/main/java/com/iuh/fit/SpringbootTechnicalPartitionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.iuh.fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTechnicalPartitionDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTechnicalPartitionDemoApplication.class, args);
+    }
+
+}
