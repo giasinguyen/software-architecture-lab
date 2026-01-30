d368624c07e816e7affdddd031fce5e951f7cdae
H82M19\Student <Student>
2026-01-30 13:44:58 +0700

    ADD: observer, adapter, composite design pattern labs

---
+++ NguyenTranGiaSi_Tuan03/design-pattern/adapter-design-pattern/src/main/java/org/iuh/fit/XMLtoJSONAdapter.java
@@ -0,0 +1,2 @@
+package org.iuh.fit;public class XMLtoJSONAdapter {
+}
---
+++ NguyenTranGiaSi_Tuan03/design-pattern/composite-design-pattern/src/main/java/org/iuh/fit/File.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class File implements FileSystemComponent {
+    private String name;
+
+    public File(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void showInfo() {
+        System.out.println("File: " + name);
+    }
+}
+
